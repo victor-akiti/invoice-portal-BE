f67191ecf695829b8600d3a49383a5662ad8a2a2
Victor Izu-Akiti <victor.izu-akiti@amni.com>
2025-10-08 16:16:16 +0100

    refactor: hide security and API keys

--- auth/initializeFirebase.js
+++ auth/initializeFirebase.js
@@ -1,8 +1,19 @@
 const admin = require("firebase-admin")
-const fs = require("fs")
-const path = require("path")
-const serviceKeys = fs.readFileSync(path.join(__dirname, "../files/misc/firebaseServiceKeys.json"));
-var serviceAccount = JSON.parse(serviceKeys);
+require("dotenv").config();
+// const serviceKeys = fs.readFileSync(path.join(__dirname, "../files/misc/firebaseServiceKeys.json"));
+
+const serviceAccount = {
+  type: process.env.GOOGLE1_TYPE,
+  project_id: process.env.GOOGLE1_PROJECT_ID,
+  private_key_id: process.env.GOOGLE1_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE1_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE1_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE1_CLIENT_ID,
+  auth_uri: process.env.GOOGLE1_AUTH_URI,
+  token_uri: process.env.GOOGLE1_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE1_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE1_CLIENT_X509_CERT_URL,
+};
 
 
 admin.initializeApp({
--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,6 +1,20 @@
 const admin = require("firebase-admin");
 const { getFirestore } = require("firebase-admin/firestore");
-const serviceAccount = require("../config/serviceAccountKey.json");
+require("dotenv").config();
+
+const serviceAccount = {
+  type: process.env.GOOGLE2_TYPE,
+  project_id: process.env.GOOGLE2_PROJECT_ID,
+  private_key_id: process.env.GOOGLE2_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE2_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE2_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE2_CLIENT_ID,
+  auth_uri: process.env.GOOGLE2_AUTH_URI,
+  token_uri: process.env.GOOGLE2_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE2_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE2_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE2_UNIVERSE_DOMAIN,
+};
 
 let staffPortalApp;
 if (!admin.apps.some(app => app.name === "staff-portal")) {
